702fac8d5c16c2b570842993b14c296db642660c
Muhammad Areeb <92858750+waybig125@users.noreply.github.com>
2023-11-24 13:39:51 +0000

    feat: Added visual designer section with mouse hover effect
    Added a new section to display a visual designer description with a mouse hover effect.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,21 @@
 "use client";
+// import Tilt from "react-tilt";
+// import { motion } from "framer-motion";
+// import { styles } from "@/app/styles";
+// import { services } from "@/constants";
+// import { fadeIn, textVariant } from "../utils/motions";
+// import SectionWrapper from "@/hoc/SectionWrapper";
+// import ServiceCard from "./ServiceCard";
 
-import Tilt from "react-tilt";
+import styles from "./about.module.scss";
+import { useState } from "react";
 import { motion } from "framer-motion";
-import { styles } from "@/app/styles";
-import { services } from "@/constants";
-import { fadeIn, textVariant } from "../utils/motions";
-import SectionWrapper from "@/hoc/SectionWrapper";
-import ServiceCard from "./ServiceCard";
+import useMousePosition from "./utils/useMousePosition";
 
 const About = () => {
+  const [isHovered, setIsHovered] = useState(false);
+  const { x, y } = useMousePosition();
+  const size = isHovered ? 400 : 40;
   return (
     <section className={`snapped`}>
       {/* <motion.div variants={() => textVariant()}>
@@ -29,6 +36,36 @@ const About = () => {
         <ServiceCard key={service.title} index={index} {...service} />
         ))}
       </div> */}
+
+      <main className={styles.main}>
+        <motion.div
+          className={styles.mask}
+          animate={{
+            WebkitMaskPosition: `${x - size / 2}px ${y - size / 2}px`,
+            WebkitMaskSize: `${size}px`,
+          }}
+          transition={{ type: "tween", ease: "backOut", duration: 0.5 }}
+        >
+          <p
+            onMouseEnter={() => {
+              setIsHovered(true);
+            }}
+            onMouseLeave={() => {
+              setIsHovered(false);
+            }}
+          >
+            A visual designer - with skills that haven't been replaced by A.I
+            (yet) - making good shit only if the paycheck is equally good.
+          </p>
+        </motion.div>
+
+        <div className={styles.body}>
+          <p>
+            I'm a <span>selectively skilled</span> product designer with strong
+            focus on producing high quality & impactful digital experience.
+          </p>
+        </div>
+      </main>
     </section>
   );
 };
